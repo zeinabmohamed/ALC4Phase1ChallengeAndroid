1907a61f96040ba48c9f09267ad48a1cde06eacf
Zeinab Mohamed Abdelmawla <zeinabmohamed22@gmail.com>
2019-07-14 19:34:12 +0200

    migrate to androidX update widgets to material design
    Signed-off-by: Zeinab Mohamed Abdelmawla <zeinabmohamed22@gmail.com>
    
    Took 1 hour 3 minutes

---
+++ app/src/main/java/com/zm/org/alc4phase1challengeandroid/ActivityA.kt
@@ -0,0 +1,12 @@
+package com.zm.org.alc4phase1challengeandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityA : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activitya)
+    }
+}
